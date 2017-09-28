7bed4a8b8fe2f9e535ae28fa0b6423eb4516a42a
Luke Herrington <lherrington100@gmail.com>
2017-09-28 10:26:22 -0700

    Add size-limit.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'dub-step.js',
-    libraryTarget: 'commonjs2', // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+    libraryTarget: 'commonjs2',
   },
   module: {
     rules: [
